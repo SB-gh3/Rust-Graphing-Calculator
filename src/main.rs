8bfcd38d13ca4ff24b572f1b1121ae6964260ca2
Spencer <spencer.e.bailey.3@gmail.com>
2024-10-13 16:04:34 -0400

    Added parent functions

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,59 @@ fn display_graph(x : Vec<f32>, y : Vec<f32>) -> Figure
     return fg;
 }
 
+fn parabolic(x : Vec<f32>, mut y : Vec<f32>) -> Vec<f32>
+{
+    for i in x.into_iter()
+    {
+        y.push(i.powi(2));
+    }
+
+    return y;
+}
+
+fn sqrt(x : Vec<f32>, mut y : Vec<f32>) -> Vec<f32>
+{
+    for i in x.into_iter()
+    {
+        if !i.sqrt().is_nan()
+        {
+            y.push(i.sqrt());
+        }
+        else
+        {
+            y.push(0.);
+        }
+    }
+
+    return y;
+}
+
+fn exponential(x : Vec<f32>, mut y : Vec<f32>) -> Vec<f32>
+{
+    for i in x.into_iter()
+    {
+        y.push((2 as f32).powi(i as i32));
+    }
+
+    return y;
+}
+
+fn reciprocal(x : Vec<f32>, mut y : Vec<f32>) -> Vec<f32>
+{
+    for i in x.into_iter()
+    {
+        y.push(1./i);
+    }
+
+    return y;
+}
+
 fn main() 
 {
     let mut x : Vec<f32> = Vec::new();
     let mut input = String::new();
+    let mut y : Vec<f32> = Vec::new();
+    let mut fg : Figure = Figure::new();
 
     println!("Enter min-x value: ");
     io::stdin().read_line(&mut input).expect("Failed to read input");
@@ -38,12 +87,26 @@ fn main()
         x.push(n as f32);
     }
 
-    let mut y : Vec<f32> = Vec::new();
-    let mut fg : Figure = Figure::new();
+    println!("Parent Function (parabolic, sqrt, exponential, reciprocal): ");
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+    let choice : String = input.trim().parse().expect("Invalid input");
+    input.clear();
 
-    for i in x.clone().into_iter()
+    if choice == "parabolic"
     {
-        y.push(i.powi(2))
+        y = parabolic(x.clone(), y.clone());
+    }
+    else if choice == "sqrt"
+    {
+        y = sqrt(x.clone(), y.clone());
+    }
+    else if choice == "exponential"
+    {
+        y = exponential(x.clone(), y.clone());
+    }
+    else if choice == "reciprocal"
+    {
+        y = reciprocal(x.clone(), y.clone());
     }
 
     fg = display_graph(x, y);
